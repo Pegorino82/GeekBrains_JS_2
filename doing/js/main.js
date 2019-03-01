91b1a9b2a15f68ba35b5590e992ac4bed37211f7
pegorino <eshkryabin@yandex.ru>
2019-03-01 14:03:54 +0300

    done

--- doing/js/main.js
+++ doing/js/main.js
@@ -1,64 +1,103 @@
+// 1. Переделайте makeGETRequest() так, чтобы она использовала промисы.
+// 2. Добавьте в соответствующие классы методы добавления товара в корзину, удаления товара из корзины и получения списка товаров корзины.
+// 3* Переделайте GoodsList так, чтобы fetchGoods() возвращал промис, а render() вызывался в обработчике этого промиса.
+
+const API = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
+
 const settings = {
     cartClass: 'cart',
+    cartWidgetClass: 'widget-cart',
     cartButtonClass: 'btn-cart',
     productsContainerClass: 'products',
     productItemClass: 'product-item',
     productImageClass: 'product-image',
 };
 
+class Main {
+    constructor(container = '.products',
+                productItemClass = 'product-item',
+                productImageClass = 'product-image',
+                cartClass = 'cart',
+                cartButtonClass = 'btn-cart',
+                cartWidgetClass = 'widget-cart',) {
+        this.ProductList = new ProductsList(container);
+        this.Cart = new Cart(cartClass, cartButtonClass, cartWidgetClass);
+
+        this._productBtnsListener();
+        this._cartItemsBtnsListener();
+    }
+
+    /**
+     * обработчик на кнопки добавления/удаления на карточке продукта
+     * @private
+     */
+    _productBtnsListener() {
+        this.ProductList.container.addEventListener('click', event => {
+            event.stopImmediatePropagation();
+            if (event.target.tagName === 'BUTTON') {
+                let item = event.target.dataset;
+                if (event.target.getAttribute('class') === 'buy-btn') {
+                    this.Cart.addItem(item);
+                } else if (event.target.getAttribute('class') === 'dec-btn') {
+                    this.Cart.removeItem(item);
+                }
+            }
+        })
+    }
+
+    /**
+     * обработчик на добавление/удаление в корзине
+     * @private
+     */
+    _cartItemsBtnsListener() {
+        this.Cart.itemsTable.addEventListener('click', event => {
+            event.stopImmediatePropagation();
+            if (event.target.tagName === 'BUTTON') {
+                let item = event.target.dataset;
+                if (event.target.getAttribute('class') === 'buy-btn') {
+                    this.Cart.incItem(item);
+                } else if (event.target.getAttribute('class') === 'dec-btn') {
+                    this.Cart.decItem(item);
+                }
+            }
+        })
+    }
+}
+
 class ProductsList {
-    constructor(container = '.products') {
-        this.container = container;
+    constructor(container = '.products', productClass = Product, rout = '/catalogData.json') {
+        this.container = document.querySelector(container);
+        this.rout = `${API}${rout}`;
         this.fetchedGoods = [];
-        this.allProducts = [];
+        this.ProductClass = productClass;
         this._fetchProducts();
-        this._fillAllProducts();
     }
 
     _fetchProducts() {
-        this.fetchedGoods = [
-            {id: 1, title: 'Notebook', price: 2000},
-            {id: 2, title: 'Mouse', price: 20},
-            {id: 3, title: 'Keyboard', price: 35},
-            {id: 4, title: 'Gamepad', price: 58},
-            {id: 5, title: 'Chair', price: 200},
-            {id: 6, title: 'Light', price: 200},
-            {id: 7, title: 'Basket', price: 20},
-            {id: 8, title: 'Something Else', price: 200},
-        ];
-    }
-
-    _fillAllProducts() {
-        for (let product of this.fetchedGoods) {
-            let newProduct = new Product(product);
-            this.allProducts.push(newProduct);
-        }
-    }
-
-    // это наверное подразумевалось для корзины?
-    getTotalCost() {
-        let totalCost = this.allProducts.reduce(
-            (accumulator, currentValue) => {
-                return accumulator + currentValue.price;
-            },
-            0
-        );
-        return `total cost: ${totalCost}`
+        fetch(this.rout)
+            .then(result => result.json())
+            .then(data => {
+                this.fetchedGoods = data;
+                this.render()
+            })
+            .catch(error => console.log(error))
     }
 
     render() {
-        let container = document.querySelector(this.container);
-        // container.innerHTML = this.allProducts.map(item => item.render()).join('');
-        container.insertAdjacentHTML('beforeend', this.allProducts.map(item => item.render()).join(''));
+        for (let prd of this.fetchedGoods) {
+            let product = new this.ProductClass(prd);
+            this.container.insertAdjacentHTML('beforeend', product.render());
+        }
     }
 }
 
 class Product {
-    constructor(item, img = 'https://placehold.it/200x150',
+    constructor(item,
+                img = 'https://placehold.it/200x150',
                 productItemClass = 'product-item',
                 productImageClass = 'product-image') {
-        this.id = item.id;
-        this.title = item.title;
+        this.id = item.id_product;
+        this.title = item.product_name;
         this.price = item.price;
         this.img = img;
         this.productItemClass = productItemClass;
@@ -72,53 +111,97 @@ class Product {
                             style="background-image: url('${this.img}');
                             background-size: cover"></div>
                         <p>$ ${this.price}</p>
-                        <button
-                        data-id="${this.id}"
+                        <button class="dec-btn"
+                        data-id_product="${this.id}"
                         data-price="${this.price}"
-                        data-title="${this.title}"
-                        >Купить</button>
+                        data-product_name="${this.title}"
+                        >Убрать из корзины</button>
+                        <button class="buy-btn"
+                        data-id_product="${this.id}"
+                        data-price="${this.price}"
+                        data-product_name="${this.title}"
+                        >Положить в корзину</button>
                     </div>`
     }
 }
 
 class Cart {
-    constructor(cartClass = 'cart', cartButtonClass = 'btn-cart',) {
+    constructor(cartClass = 'cart', cartButtonClass = 'btn-cart', cartWidgetClass = 'widget-cart') {
         this.cartClass = cartClass;
-        this.cartButtonClass = cartButtonClass;
-        this.itemsList = [];
+        this.cartButton = document.querySelector(`.${cartButtonClass}`);
+        this.cartWidget = document.querySelector(`.${cartWidgetClass}`);
+        this.itemsTable = this.cartWidget.querySelector('table'); // список товров в корзине на странице
+        this.itemsTableBody = this.itemsTable.querySelector('tbody'); // список товров в корзине на странице
+        this.itemsList = {}; // {prod_id: {price: , title: , quantity:}, ...}
+        this._fillAllCartItems();
+        this._showCart();
     }
 
     /**
      * добавляет товар в корзину
      */
     addItem(item) {
-        let idx = this.itemsList.indexOf(item);
-        if (idx !== -1) {
-            this.itemsList[idx].inc()
+        if (item.id_product in this.itemsList) {
+            this.incItem(item);
         } else {
-            this.itemsList.push(item)
+            this.itemsList[item.id_product] = {
+                'price': item.price,
+                'product_name': item.product_name,
+                'quantity': 1
+            };
+            // отрисовка добавленного товара
+            this._renderCartItem(item.id_product, this.itemsList[item.id_product]);
         }
-        console.log('add', this.itemsList) // TODO for debug
+        // console.log('add', this.itemsList) //
     }
 
     /**
      * удаляет товар из корзины
      */
     removeItem(item) {
-        let idx = this.itemsList.indexOf(item);
-        if (idx !== -1 && this.itemsList[idx].quantity > 0) {
-            this.itemsList[idx].dec()
-        } else if (idx !== -1 && this.itemsList[idx].quantity === 0) {
-            this.itemsList.splice(idx, 1)
+        if (item.id_product in this.itemsList) {
+            delete this.itemsList[item.id_product];
+            let neededItem = this.itemsTable.querySelector(`#order-item_${item.id_product}`);
+            neededItem.remove();
+            this._showTotalQuantity();
+            this._showTotalCost()
         }
-        console.log('rem', this.itemsList)// TODO for debug
+        // console.log('rem', this.itemsList)//
     }
+
+
+    /**
+     * увеличивает количество товара в корзине
+     */
+    incItem(item) {
+        this.itemsList[item.id_product]['quantity']++;
+        this._renderCartItem(item.id_product, this.itemsList[item.id_product]);
+        // console.log('inc', this.itemsList)
+    }
+
+    /**
+     * уменьшает количество товара в корзине
+     */
+    decItem(item) {
+        if (this.itemsList[item.id_product]['quantity'] > 1) {
+            this.itemsList[item.id_product]['quantity']--;
+            this._renderCartItem(item.id_product, this.itemsList[item.id_product]);
+        } else {
+            delete this.itemsList[item.id_product];
+            let neededItem = this.itemsTable.querySelector(`#order-item_${item.id_product}`);
+            neededItem.remove();
+            this._showTotalQuantity();
+            this._showTotalCost()
+        }
+        // console.log('dec', this.itemsList)
+    }
+
 
     /**
      * возвращает полную стоимость корзины
      */
-    getTotalCost() {
-        return this.itemsList.reduce(
+    _getTotalCost() {
+        return Object.values(this.itemsList).reduce(
             (accumulator, currentValue) => {
                 return accumulator + currentValue.price * currentValue.quantity;
             },
@@ -129,13 +212,97 @@ class Cart {
     /**
      * возвращает общее количество товаров в корзине
      */
-    getTotalQuantity() {
-        return this.itemsList.reduce(
+    _getTotalQuantity() {
+        return Object.values(this.itemsList).reduce(
             (accumulator, currentValue) => {
                 return accumulator + currentValue.quantity;
             },
             0
         );
+    }
+
+    _renderCartItem(id, item) {
+        let neededItem = this.itemsTable.querySelector(`#order-item_${id}`);
+        if (neededItem) {
+            neededItem.innerHTML =
+                `<td>${item.product_name}</td>
+                    <td>${item.price}</td>
+                    <td>${item.quantity}</td>
+                    <td>${item.price * item.quantity}</td>
+                    <td><button class="buy-btn"
+                        data-id_product="${id}"
+                        data-price="${item.price}"
+                        data-product_name="${item.product_name}">+</button></td>
+                    <td><button class="dec-btn"
+                        data-id_product="${id}"
+                        data-price="${item.price}"
+                        data-product_name="${item.product_name}">-</button></td>`
+        } else {
+            const rowsItems = this.itemsTable.querySelector('tbody');
+            rowsItems.insertAdjacentHTML('beforeend',
+                `
+                <tr id="order-item_${id}">
+                    <td>${item.product_name}</td>
+                    <td>${item.price}</td>
+                    <td>${item.quantity}</td>
+                    <td>${item.price * item.quantity}</td>
+                    <td><button class="buy-btn"
+                        data-id_product="${id}"
+                        data-price="${item.price}"
+                        data-product_name="${item.product_name}">+</button></td>
+                    <td><button class="dec-btn"
+                        data-id_product="${id}"
+                        data-price="${item.price}"
+                        data-product_name="${item.product_name}">-</button></td>
+                </tr>`
+            )
+        }
+        this._showTotalQuantity();
+        this._showTotalCost()
+    }
+
+    _fillAllCartItems() {
+        for (let ptod_id in this.itemsList) {
+            let poduct = this.itemsList[ptod_id];
+            this.itemsTableBody.insertAdjacentHTML('beforeend', `
+                <tr id="order-item_${ptod_id}">
+                    <td>${poduct['product_name']}</td>
+                    <td>${poduct['price']}</td>
+                    <td>${poduct['quantity']}</td>
+                    <td>${poduct['price'] * poduct['quantity']}</td>
+                    <td><button class="buy-btn"
+                        data-id_product="${ptod_id}"
+                        data-price="${poduct['price']}"
+                        data-product_name="${poduct['product_name']}">+</button></td>
+                    <td><button class="dec-btn"
+                        data-id_product="${ptod_id}"
+                        data-price="${poduct['price']}"
+                        data-product_name="${poduct['product_name']}">-</button></td>
+                </tr>`)
+        }
+    }
+
+    _showTotalQuantity() {
+        const totalQuantityTable = this.itemsTable.querySelector('#total-goods');
+        totalQuantityTable.innerHTML = this._getTotalQuantity();
+        const totalQuantityBtn = this.cartButton.querySelector('#btn-cart-total-goods');
+        totalQuantityBtn.innerHTML = this._getTotalQuantity();
+    }
+
+    _showTotalCost() {
+        const totalCostTable = this.itemsTable.querySelector('#total-price');
+        totalCostTable.innerHTML = this._getTotalCost();
+        const totalCostBtn = this.cartButton.querySelector('#btn-cart-total-price');
+        totalCostBtn.innerHTML = this._getTotalCost();
+    }
+
+    _showCart() {
+        this.cartButton.addEventListener('click', () => {
+            this.cartWidget.style.display = 'block';
+        });
+        this.cartWidget.querySelector('#close-cart-btn').addEventListener('click', () => {
+            this.cartWidget.style.display = 'none';
+        })
     }
 }
 
@@ -178,21 +345,7 @@ class CartItem {
     }
 }
 
-const pList = new ProductsList();
-pList.render();
-// console.log(pList.getTotalCost());
-let item = {id: 1, title: 'Notebook', price: 2000};
-let item2 = {id: 7, title: 'Basket', price: 20};
-let product = new Product(item);
-let product2 = new Product(item2);
-let product3 = new Product(item); // вот такое спокойно проходит, тут будет два "разных" продукта
-let cartItem = new CartItem(product);
-let cartItem2 = new CartItem(product2);
-let cartItem3 = new CartItem(product3);
-let cartItem4 = new CartItem(product); // и такое тоже
-let cart = new Cart();
-cart.addItem(cartItem);
-cart.addItem(cartItem);
-cart.addItem(cartItem2);
-cart.addItem(cartItem3);
-cart.addItem(cartItem4);
+// const pList = new ProductsList();
+// let cart = new Cart();
+
+new Main();
