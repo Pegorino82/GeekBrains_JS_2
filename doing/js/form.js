1f1037c8ef21aa679e3f22759e7839ed47199ad1
pegorino <eshkryabin@yandex.ru>
2019-03-04 10:49:24 +0300

    task 3 done

--- doing/js/form.js
+++ doing/js/form.js
@@ -1,21 +1,55 @@
 const settings = {
     formSelector: '.form',
     formFieldSelector: '.form-field',
+    nonValidateClass: 'alert'
 
 };
 
 let form = document.querySelector(settings.formSelector);
 form.addEventListener('submit', (event) => {
-    let fields = event.target.querySelectorAll(`${settings.formFieldSelector}[type=text]`);
-    fields.forEach((el) => {
-        console.log(el.name, isValid(el));
-        if (!isValid(el)) {
-            el.classList.add('alert');
-            event.preventDefault();
-        }
-    })
+    if (!formIsValid(form)) {
+        event.preventDefault();
+    }
 });
 
+/**
+ * проверяет что вся форма валидна
+ * @param form
+ * @return {boolean}
+ */
+function formIsValid(form) {
+    let isValid = true;
+    const fields = form.querySelectorAll('input');
+    fields.forEach((el) => {
+        console.log(el.name, fieldIsValid(el));
+        if (!fieldIsValid(el)) {
+            el.classList.add(settings.nonValidateClass);
+            let alertMessage = form.querySelector(`div[name=${el.name}]`);
+            alertMessage.classList.toggle('field-is-valid');
+            isValid = false
+        }
+    });
+    return isValid
+}
+
+/**
+ * проверяет поле формы на валидность
+ * @param elem
+ * @return {*}
+ */
+function fieldIsValid(elem) {
+    if (elem.name === 'name') {
+        return isValidName(elem.value)
+    } else if (elem.name === 'phone') {
+        return isValidPhone(elem.value)
+    } else if (elem.name === 'email') {
+        return isValidEmail(elem.value)
+    } else if (elem.name === 'text') {
+        return isValidText(elem.value)
+    } else return true
+}
+
+// валидаторы
 function isValidName(name) {
     return name.match(/^[а-яА-ЯёЁa-zA-Z]+$/g) ? true : false
 }
@@ -32,14 +66,3 @@ function isValidText(text) {
     return text ? true : false
 }
 
-function isValid(elem) {
-    if (elem.name === 'name') {
-        return isValidName(elem.value)
-    } else if (elem.name === 'phone') {
-        return isValidPhone(elem.value)
-    } else if (elem.name === 'email') {
-        return isValidEmail(elem.value)
-    } else if (elem.name === 'text') {
-        return isValidText(elem.value)
-    } else return true
-}
